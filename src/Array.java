4e8b067447d6db496ee0484290c91e119d544430
mweems <Thoughtworker@MW9591.local>
2013-08-30 12:03:06 -0400

    added array exercise

---
+++ src/Array.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+public class Array {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String shortestString(String[] strings) {
+        String shortest = strings[0];
+        for (String word : strings){
+            if (word.length() < shortest.length()){
+                shortest = word;
+            }
+        }
+        return shortest;
+    }
+
+    public Integer[] computeNumbers(Integer[] numbers, String type) {
+        ArrayList<Integer> values = new ArrayList<Integer>();
+        int oddOrEven;
+        if (type == "odd"){
+            oddOrEven = 1;
+        } else {
+            oddOrEven = 0;
+        }
+        for (int num : numbers) {
+            if (num%2 == oddOrEven) {
+                values.add(num);
+            }
+        }
+        Integer[] num = new Integer[values.size()];
+        values.toArray(num);
+        return num;
+    }
+
+    public Integer[] multiples(Integer[] numbers) {
+        Integer[] odd = computeNumbers(numbers, "odd");
+        Integer[] even = computeNumbers(numbers, "even");
+        Integer[] result = new Integer[2];
+        result[0] = add(odd);
+        result[1] = multiply(even);
+        return result;
+    }
+
+    private Integer add(Integer[] numbers) {
+        int sum = 0;
+        for(int i = 0; i < numbers.length; i++){
+            sum+= numbers[i];
+        }
+        return sum;
+    }
+
+    private Integer multiply(Integer[] numbers) {
+        int product = 1;
+        for(int i = 0; i < numbers.length; i++) {
+            product*= numbers[i];
+        }
+        return product;
+    }
+}
---
+++ src/ArrayTest.java
@@ -0,0 +1,29 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class ArrayTest {
+
+    @Test
+    public void givenAnArrayOfStringsReturnTheShortestString() {
+        Array array = new Array();
+        String[] strings = new String[] {"Bounce", "Paula", "Johnny"} ;
+        assertEquals(array.shortestString(strings), "Paula");
+    }
+
+    @Test
+    public void givenAnArrayOfIntegersPrintTheOddOnes() {
+        Array array = new Array();
+        Integer[] integers = new Integer[] {1,2,3,4,5,6,7,8,9};
+        Integer[] expected = new Integer[] {1,3,5,7,9};
+        assertEquals(array.computeNumbers(integers, "odd"), expected);
+    }
+
+    @Test
+    public void givenAnArrayOfIntegersAddTheOddOnesAndMultiplyTheEvenOnes() {
+        Array array = new Array();
+        Integer[] integers = new Integer[] {1,2,3,4,5,6,7,8,9};
+        Integer[] expected = new Integer[] {25,384}; //{ sumOfOdds, productOfEvens}
+        assertEquals(array.multiples(integers), expected);
+    }
+}
