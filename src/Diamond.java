42eb11a18c2c4f7b53373b8592787769760ed61c
mweems <Thoughtworker@MW9591.local>
2013-09-03 13:33:59 -0400

    fixed part two of the diamond exercise

--- src/Diamond.java
+++ src/Diamond.java
@@ -1,33 +1,51 @@
 public class Diamond {
 
-    String triangle = "";
+    String shape = "";
 
     public String triangle(int num) {
         if (num%2 == 0){
             return "Must enter an odd number";
         } else {
-            for (int i = 1; i <= num;i++ ){
-                getSpaces(i,num);
+            for (int i = 1; i <= num; i++){
+                getSpaces(i, num);
                 getAsterisks(i);
             }
-        return triangle;
+        return shape;
         }
     }
 
+    public String diamond(int num) {
+        if (num%2 == 0) {
+            return "Must enter an odd number";
+        } else {
+            int totalSize = (num + 1) / 2;
+            for (int i = 1; i <= totalSize; i++){
+                getSpaces(i, totalSize);
+                getAsterisks(i);
+            }
+            if (totalSize != 1) {
+                for (int i = (totalSize - 1); i > 0; i--){
+                    getSpaces(i, totalSize);
+                    getAsterisks(i);
+                }
+            }
+        }
+        return shape;
+    }
+
+
     private void getSpaces(int lineNumber, int totalLines){
         for (int i = lineNumber; i < totalLines; i++){
-            triangle += " ";
+            shape += " ";
         }
     }
 
     private void getAsterisks(int lineNumber){
             lineNumber *= 2;
-            for (int i = 1; i < (lineNumber); i++) {
-                triangle += "*";
+            for (int i = 1; i < lineNumber; i++) {
+                shape += "*";
             }
-        triangle += "\n";
+        shape += "\n";
     }
-
-
 
 }
--- src/DiamondTest.java
+++ src/DiamondTest.java
@@ -6,20 +6,44 @@ public class DiamondTest {
 
 
     @Test
-    public void shouldPrintASingleAsteriskWhenPassedAOne() {
+    public void TriangleShouldReturnASingleAsteriskWhenPassedAOne() {
         Diamond diamond = new Diamond();
         assertEquals("*\n", diamond.triangle(1));
     }
 
     @Test
-    public void shouldPrintErrorMessageIfNumberGivenIsAnEvenNumber() {
+    public void TriangleShouldReturnErrorMessageIfNumberGivenIsAnEvenNumber() {
         Diamond diamond = new Diamond();
         assertEquals("Must enter an odd number", diamond.triangle(2));
     }
 
     @Test
-    public void shouldPrintAsterisksInTheShapeOfATriangleAsTallAsThePassedInNumber() {
+    public void TriangleShouldReturnAsterisksInACenteredTriangle3RowsHigh() {
         Diamond diamond = new Diamond();
         assertEquals("  *\n ***\n*****\n",diamond.triangle(3));
+    }
+
+    @Test
+    public void DiamondShouldReturnASingleAsteriskWhenPassedAOne() {
+        Diamond diamond = new Diamond();
+        assertEquals("*\n", diamond.diamond(1));
+    }
+
+    @Test
+    public void DiamondShouldReturnErrorMessageIfNumberGivenIsAnEvenNumber() {
+        Diamond diamond = new Diamond();
+        assertEquals("Must enter an odd number", diamond.diamond(2));
+    }
+
+    @Test
+    public void DiamondShouldReturnAsterisksInACenteredDiamond3RowsHigh() {
+        Diamond diamond = new Diamond();
+        assertEquals(" *\n***\n *\n", diamond.diamond(3));
+    }
+
+    @Test
+    public void DiamondShouldReturnAsterisksInACenteredDiamond5RowsHigh() {
+        Diamond diamond = new Diamond();
+        assertEquals("  *\n ***\n*****\n ***\n  *\n", diamond.diamond(5));
     }
 }
