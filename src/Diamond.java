76b5294cf18c7085e259e20000077a935585469d
mweems <Thoughtworker@MW9591.local>
2013-09-03 10:59:14 -0400

    finished step one of diamond problem

--- src/Diamond.java
+++ src/Diamond.java
@@ -6,22 +6,26 @@ public class Diamond {
         if (num%2 == 0){
             return "Must enter an odd number";
         } else {
-        for (int i = 1; i <= num;i++ ){
-            getSpaces(i,num);
-            for (int j = 0; j < i; j++){
-               triangle += "*";
-            };
-            triangle += "\n";
-        }
+            for (int i = 1; i <= num;i++ ){
+                getSpaces(i,num);
+                getAsterisks(i);
+            }
         return triangle;
         }
     }
 
     private void getSpaces(int lineNumber, int totalLines){
-        String space = " ";
         for (int i = lineNumber; i < totalLines; i++){
-            triangle += space;
+            triangle += " ";
         }
+    }
+
+    private void getAsterisks(int lineNumber){
+            lineNumber *= 2;
+            for (int i = 1; i < (lineNumber); i++) {
+                triangle += "*";
+            }
+        triangle += "\n";
     }
 
 
