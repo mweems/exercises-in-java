2294d6a52a045a80e529b592bb713a109f2873e6
mweems <Thoughtworker@MW9591.local>
2013-09-03 10:11:28 -0400

    added a few new tests, still incomplete

--- src/Diamond.java
+++ src/Diamond.java
@@ -1,16 +1,29 @@
 public class Diamond {
 
+    String triangle = "";
+
     public String triangle(int num) {
-        String triangle = "";
-        String[] string = new String[num];
+        if (num%2 == 0){
+            return "Must enter an odd number";
+        } else {
         for (int i = 1; i <= num;i++ ){
+            getSpaces(i,num);
             for (int j = 0; j < i; j++){
                triangle += "*";
             };
             triangle += "\n";
         }
-        string = triangle.split("\n");
-        System.out.println("+++" + string[2].length());
-        return "";
+        return triangle;
+        }
     }
+
+    private void getSpaces(int lineNumber, int totalLines){
+        String space = " ";
+        for (int i = lineNumber; i < totalLines; i++){
+            triangle += space;
+        }
+    }
+
+
+
 }
--- src/DiamondTest.java
+++ src/DiamondTest.java
@@ -1,12 +1,25 @@
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
+import static junit.framework.Assert.assertEquals;
 
 public class DiamondTest {
+
+
+    @Test
+    public void shouldPrintASingleAsteriskWhenPassedAOne() {
+        Diamond diamond = new Diamond();
+        assertEquals("*\n", diamond.triangle(1));
+    }
+
+    @Test
+    public void shouldPrintErrorMessageIfNumberGivenIsAnEvenNumber() {
+        Diamond diamond = new Diamond();
+        assertEquals("Must enter an odd number", diamond.triangle(2));
+    }
 
     @Test
     public void shouldPrintAsterisksInTheShapeOfATriangleAsTallAsThePassedInNumber() {
         Diamond diamond = new Diamond();
-        assertEquals(diamond.triangle(3), "  *  \n *** \n*****\n");
+        assertEquals("  *\n ***\n*****\n",diamond.triangle(3));
     }
 }
